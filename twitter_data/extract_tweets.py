ded53b270b9082fb5755ba50b3348c6cb029960b
mohammadmahdi <mma137421@gmail.com>
2020-09-30 11:44:38 +0330

    Add twitter data extractor

---
+++ twitter_data/extract_tweets.py
@@ -0,0 +1,42 @@
+import json
+import re
+
+link_pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+id_pattern = re.compile("@[A-Za-z0-9-_]+")
+emoji_pattern = re.compile(pattern="["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00010000-\U0010ffff"
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   "]+", flags=re.UNICODE)
+
+# %%
+with open('twitter_data/result.json') as f:
+    crawled_data = json.load(f)
+
+# %%
+result = {}
+with open('twitter_data/extracted_data.txt', 'w') as f:
+    for entity in crawled_data:
+        for keyword in entity.keys():
+            keyword_result = {}
+            for key, tweet in entity[keyword]['globalObjects']['tweets'].items():
+                text = tweet['full_text']
+                lang = tweet['lang']
+
+                keyword_result.update(
+                    {key: {'full_text': text,
+                           'lang': lang,
+                           }
+                     }
+                )
+                if lang == 'fa':
+                    removed_link = link_pattern.sub('', text)
+                    removed_username = id_pattern.sub('', removed_link)
+                    removed_hashtags = removed_username.replace('#', '')
+                    removed_emoji = emoji_pattern.sub('', removed_hashtags)
+                    f.write(removed_emoji + '\n\n\n')
+            result.update({keyword: keyword_result})
